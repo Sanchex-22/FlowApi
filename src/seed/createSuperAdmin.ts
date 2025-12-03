5451bd82cb34124dbfa647b611900f29b2632f91
david@intermaritime.org <david@intermaritime.org>
2025-12-03 16:55:05 -0500

    fix prisma error js seed

--- src/seed/createSuperAdmin.ts
+++ src/seed/createSuperAdmin.ts
@@ -1,6 +1,6 @@
 
 import bcrypt from 'bcryptjs';
-import prisma from '../../lib/prisma';
+import prisma from '../../lib/prisma.js';
 
 export async function createSuperAdminSeed() {
   const passwordHash = await bcrypt.hash('SuperAdmin123!', 10);
